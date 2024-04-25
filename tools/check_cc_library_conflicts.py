82b1df57a5a17da4f8c1cae785d6460c9f313f77
Ezekiel Warren <ezekiel@seaube.com>
2024-04-25 14:50:30 -0700

    refactored script to only run on certain modules

--- tools/check_cc_library_conflicts.py
+++ tools/check_cc_library_conflicts.py
@@ -1,54 +1,67 @@
 import os
 import subprocess
 import tempfile
+import itertools
+import sys
+import argparse
+import json
+import shutil
+from enum import Enum
 from pathlib import Path
 
-import click
 from registry import RegistryClient
 
+RED = "\x1b[31m"
+GREEN = "\x1b[32m"
+RESET = "\x1b[0m"
 
-@click.command()
-@click.argument("module")
-@click.option("--version")
-@click.option("--registry", default=".")
-def check_cc_library_conflicts(module, version, registry):
+class TargetBuildStatus(Enum):
+    NONE = 0
+    SUCCESS = 1
+    FAILURE = 2
+
+COLOR = {
+  TargetBuildStatus.NONE: "",
+  TargetBuildStatus.SUCCESS: GREEN,
+  TargetBuildStatus.FAILURE: RED,
+}
+
+class TargetBuildResult:
+    description = ""
+    status = TargetBuildStatus.NONE
+
+    def __init__(self, description):
+        self.description = description
+
+    def print_result(self):
+        print(f"{COLOR[self.status]}{self.status}\t\t{self.description}{RESET}")
+
+def check_cc_library_conflicts(modules, registry):
     """
-    Check if a modules cc_library targets conflict with any other modules in
-    the registry.
+    Check if a modules cc_library targets conflict with other modules in the
+    registry.
     """
     client = RegistryClient(registry)
-    if not client.contains(module):
-        raise click.BadParameter(
-            f"{module=} not found in {registry=}. "
-            f"Possible modules: {', '.join(client.get_all_modules())}"
-        )
-    client.update_versions(module)
-    versions = [ver for _, ver in client.get_module_versions(module)]
-    version = version or versions[-1]
-    if not client.contains(module, version):
-        raise click.BadParameter(
-            f"{version=} not found for {module=}. "
-            f"Possible versions: {', '.join(versions)}"
-        )
-    
-    # modules that require more attention to test against
-    # TODO: Support these modules
-    problem_modules = {"rules_rust"}
+    for module in modules:
+        if not client.contains(module):
+            raise click.BadParameter(
+                f"{module=} not found in {registry=}. "
+                f"Possible modules: {', '.join(client.get_all_modules())}"
+            )
+
+        mod_versions = [ver for _, ver in client.get_module_versions(module, include_yanked = False)]
+        if len(mod_versions) == 0:
+            raise click.BadParameter(
+                f"{module=} only contains yanked versions"
+            )
 
     tmp_dir = Path(tempfile.mkdtemp())
-    modules = []
 
     with open(tmp_dir.joinpath("MODULE.bazel"), 'w') as module_bazel_file:
-        for mod in client.get_all_modules():
-            if mod in problem_modules:
-                continue
-
-            client.update_versions(mod)
+        for mod in modules:
             mod_versions = [ver for _, ver in client.get_module_versions(mod, include_yanked = False)]
-            if len(mod_versions) > 0:
-                modules.append(mod)
-                mod_version = mod_versions[-1]
-                module_bazel_file.write(f"bazel_dep(name=\"{mod}\", version=\"{mod_version}\")\n")
+            mod_version = mod_versions[-1]
+            module_bazel_file.write(f"bazel_dep(name=\"{mod}\", version=\"{mod_version}\")\n")
 
     query_string = " + ".join([f"kind(cc_library, attr(visibility, \"//visibility:public\", @{mod}//...))" for mod in modules])
 
@@ -59,41 +72,56 @@ def check_cc_library_conflicts(module, version, registry):
         cwd = tmp_dir,
     )
     cc_library_targets = [line for line in result.stdout.split('\n') if line]
-    main_module_targets = [target for target in cc_library_targets if target.startswith(f"@{module}//")]
-
-    modules_with_cc_targets = set()
-    for target in cc_library_targets:
-        module_name = target[1:target.index("/")]
-        modules_with_cc_targets.add(module_name)
-    
-    # re-write MODULE.bazel to get rid of modules without cc_library targets
-    # this is necessary due to several errors in other moduels not allowing a
-    # build to occur
-    with open(tmp_dir.joinpath("MODULE.bazel"), 'w') as module_bazel_file:
-        for mod in modules_with_cc_targets:
-            mod_versions = [ver for _, ver in client.get_module_versions(mod, include_yanked = False)]
-            mod_version = mod_versions[-1]
-            module_bazel_file.write(f"bazel_dep(name=\"{mod}\", version=\"{mod_version}\")\n")
 
     open(tmp_dir.joinpath("dummy_main.cc"), "w").write("int main() {}\n")
 
     build_file_path = tmp_dir.joinpath("BUILD.bazel")
     count = 0
+    target_build_results = dict()
     with open(build_file_path, "w") as build_file:
-        for main_module_target in main_module_targets:
-            for target in cc_library_targets:
-                if main_module_target == target:
-                    continue
-                build_file.write(f'# {main_module_target} + {target}\n')
-                build_file.write(f'cc_binary(name="{count}", deps = ["{main_module_target}", "{target}"], srcs=["dummy_main.cc"])\n\n')
-                count += 1
+        for (a, b) in itertools.combinations(cc_library_targets, 2):
+            target_name = f"{count}"
+            target_build_results[f"//:{target_name}"] = TargetBuildResult(f"{a} + {b}")
+            build_file.write(f'# {a} + {b}\n')
+            build_file.write(f'cc_binary(name="{target_name}", deps=["{a}", "{b}"], srcs=["dummy_main.cc"])\n\n')
+            count += 1
 
+    subprocess.run(
+        ["bazel", "build", "//...", "-k", "--build_event_json_file=build.json"],
+        cwd = tmp_dir,
+    )
 
-    print(tmp_dir)
+    with open(tmp_dir.joinpath("build.json"), "r") as build_json_file:
+        while line := build_json_file.readline().rstrip():
+            msg = json.loads(line)
+            if "targetCompleted" in msg["id"]:
+                label = msg["id"]["targetCompleted"]["label"]
+                payload = msg["completed"]
+                target_build_results[label].status = TargetBuildStatus.SUCCESS if payload["success"] else TargetBuildStatus.FAILURE
 
+    for label in target_build_results:
+        target_build_results[label].print_result()
+
+    subprocess.run(
+        ["bazel", "shutdown"],
+        cwd = tmp_dir,
+    )
+
+    shutil.rmtree(tmp_dir)
 
 if __name__ == "__main__":
     # Under 'bazel run' we want to run within the source folder instead of the execroot.
     if os.getenv("BUILD_WORKSPACE_DIRECTORY"):
         os.chdir(os.getenv("BUILD_WORKSPACE_DIRECTORY"))
-    check_cc_library_conflicts()
+
+    parser = argparse.ArgumentParser(
+        prog = 'check_cc_library_conflicts',
+        description = 'Check if a modules cc_library targets conflict with other modules in the registry.'
+    )
+
+    parser.add_argument('modules', nargs = '+')
+    parser.add_argument('--registry', default = '.')
+
+    args = parser.parse_args()
+    
+    check_cc_library_conflicts(args.modules, args.registry)
